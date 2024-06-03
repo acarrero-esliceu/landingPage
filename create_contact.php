c9cbdc2dbeae35e460acdd2d0de9bc3c4df44ec6
acarrero-esliceu <acarrerov@esliceu.net>
2024-06-03 09:51:59 +0200

    crear contacto

--- create_contact.php
+++ create_contact.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'db.php';
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
 
@@ -12,7 +12,22 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "NOMBRE: ".$nombre."<br>";
     echo "TELEFONO: ".$telefono."<br>";
     echo "CORREO: ".$correo."<br>";
-    echo "MENSAJE".$mensaje."<br>" ;
+    echo "MENSAJE: ".$mensaje."<br>" ;
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES (?,?,?,?);";
+    $stmt=$conn->prepare($sql);
+    $stmt->bind_param("ssss",$nombre,$telefono,$correo,$mensaje);
+    $result = $stmt->execute();
+
+    if($result){
+        $stmt->close();
+        $conn->close();
+        echo "CONTACTO CREADO";
+        header("Location: contacto.php?status=success&msg=Mensaje Guardado!");
+
+    }else{
+        echo "ERROR AL CREAR CONTACTO";
+    }
+
 }else{
     echo "ERROR EN METODO POST";
 }
